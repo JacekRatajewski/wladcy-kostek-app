3ddc64ea1841524150f9d16ac6cfccb94ed19c46
Brodaty <jacekratajewski.98.projekty@gmail.com>
2024-05-24 09:13:11 +0200

    master: fix lib index for build

--- libs/src/index.ts
+++ libs/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/ui.module';
 export * from './lib/components/icon/icon.component'
+export * from './lib/directives/tooltip/tooltip.directive'
+export * from './lib/directives/var/var.directive'

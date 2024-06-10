91718e50ec37497267d7f9d516abd54453164b01
Brodaty <jacekratajewski.98.projekty@gmail.com>
2024-06-10 08:34:44 +0200

    feat(#8): rpg butler add new commands

--- apps/rpg-butler/src/commands/roll.ts
+++ apps/rpg-butler/src/commands/roll.ts
@@ -1,3 +1,5 @@
+import { SlashCommandBuilder } from "discord.js";
+
 export class RollService {
   private count!: number;
   dice: number;
@@ -19,3 +21,27 @@ export class RollService {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 }
+
+export const rollCommand = new SlashCommandBuilder()
+  .setName('roll')
+  .setDescription('Rzuć kością!');
+rollCommand
+  .addStringOption((option) =>
+    option
+      .setName('dice')
+      .setDescription('Wybierz kostkę!')
+      .setRequired(true)
+      .setChoices(
+        { name: 'd3', value: '1d3' },
+        { name: 'd4', value: '1d4' },
+        { name: 'd6', value: '1d6' },
+        { name: 'd8', value: '1d8' },
+        { name: 'd10', value: '1d10' },
+        { name: 'd12', value: '1d12' },
+        { name: 'd20', value: '1d20' },
+        { name: 'd100', value: '1d100' }
+      )
+  )
+  .addNumberOption((option) =>
+    option.setName('count').setDescription('Ile kości?').setRequired(false)
+  );

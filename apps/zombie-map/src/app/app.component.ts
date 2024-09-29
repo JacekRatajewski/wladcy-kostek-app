f9d8ba0e5579d0dfee8c465ef1610aea5682c536
Brodaty <jacekratajewski.98.projekty@gmail.com>
2024-09-29 12:06:28 +0200

    feat: add download button

--- apps/zombie-map/src/app/app.component.ts
+++ apps/zombie-map/src/app/app.component.ts
@@ -7,10 +7,17 @@ import { MapComponent } from './map/map.component';
 import { CommonModule } from '@angular/common';
 import { NewsComponent } from './news/news.component';
 import { LocalstorageService } from './localstorage.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MapComponent, CommonModule, NewsComponent],
+  imports: [
+    RouterModule,
+    MapComponent,
+    CommonModule,
+    NewsComponent,
+    HttpClientModule,
+  ],
   providers: [EventsService, LocalstorageService],
   selector: 'wladcy-kostek-app-root',
   templateUrl: './app.component.html',
@@ -18,11 +25,19 @@ import { LocalstorageService } from './localstorage.service';
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('vid') video!: ElementRef;
-  constructor(public router: Router, private ls: LocalstorageService) {}
+  constructor(
+    public router: Router,
+    private ls: LocalstorageService,
+    private http: HttpClient
+  ) {}
   ngAfterViewInit(): void {
     (this.video.nativeElement as HTMLVideoElement).play();
   }
   enterName(event: any) {
     this.ls.set('username', event.target.value);
+  }
+  downloadApk() {
+    const fileUrl = 'https://wtwb.xyz/apk/wtwb.apk';
+    window.open(fileUrl, '_blank');
   }
 }
